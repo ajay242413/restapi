21e29c9a1cafa5fcf87210d1403d5fcb68c73781
kumara163 <ajeya.kumar@emc.com>
2021-11-16 17:46:31 +0530

    yml changes

--- routes/health.js
+++ routes/health.js
@@ -1,18 +1,13 @@
 module.exports = {
-    getPosts(req, res) {
-        res.status(200).send(req.store.posts)
-    },
-    addPost(req, res) {
-        req.store.posts.push(req.body)
-        res.status(201).send({"id": req.store.posts.length})
+
+    checkStatus(req, res) {
+        res.status(201).send({
+            "name": req.store.posts[req.store.posts.length - 1].name,
+            "status": req.store.posts[req.store.posts.length - 1].status})
     },
     updatePost(req, res) {
-        const id = req.params.postId
-        req.store.posts[id] = req.body
-        res.status(200).send(req.store.posts[id])
-    },
-    removePost(req, res) {
-        req.store.posts.splice(req.params.postId, 1)
-        res.status(204).send()
+        var resetStatus = req.params.postId
+        req.store.posts[req.store.posts.length - 1].status = resetStatus
+        res.status(200).send('updated status')
     }
 }
